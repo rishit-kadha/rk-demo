9d3fc49c1cf2bd6554021562d36ea0dbcabdcef1
Rishit Kadha <115226865+rishit-kadha@users.noreply.github.com>
2024-08-31 13:01:48 +0530

    some changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function login(email , pass){
-    if (email == "rkadha226@gmail.com" && pass == "123"){
+    if (email == "email@examle.com" && pass == "123"){
         var newWin = window.open("interface.html", "_blank");
         newWin.onload = function(){
 
