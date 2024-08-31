91e3bee1695e7f574e1d0b4eda5d679c38e10be6
Rishit Kadha <115226865+rishit-kadha@users.noreply.github.com>
2024-08-31 12:58:33 +0530

    started writing JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function login(email , pass){
+    if (email == "rkadha226@gmail.com" && pass == "123"){
+        var newWin = window.open("interface.html", "_blank");
+        newWin.onload = function(){
+
+        }
+    }
+
+}
